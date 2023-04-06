c007762200d0dcf1b911043dccfc61d6a4e56001
rick-paaz <mercer@cs.arizona.edu>
2023-04-06 13:14:35 -0700

    Drawing again

--- MediaFX/src/PlaySongDemo/LineItem.java
+++ MediaFX/src/PlaySongDemo/LineItem.java
@@ -3,19 +3,19 @@ package PlaySongDemo;
 public class LineItem {
 
   // Adding a comment
-   int howMany;
-   double howMuch;
+  private int howMany;
+  private double howMuch;
 
   public LineItem(int quantity, double price) {
     howMuch = price;
     howMany = quantity;
-    }
+  }
 
   double lineItemTotal() {
     return Math.round(100 * howMany * howMuch) / 100.0;
   }
 
   public String toString() {
-    return howMany + "@" + howMuch + " = " + lineItemTotal();
+    return howMany + "@" + howMuch + " = $" + lineItemTotal();
   }
 }
